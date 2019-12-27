50d06684937447d3e753f95157a96b6f5d7458be
ejndi <johndoe@example.com>
2019-12-26 20:25:49 -0800

    hud

---
+++ index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pan</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Sulphur+Point&display=swap" rel="stylesheet"> 
+</head>
+<body>
+
+
+
+<div class="header">
+
+<h2 class="logo">Pan</h2>
+<input type="checkbox" id="chk">
+<label for="chk" class="show-menu-btn">
+    <i class="fas fa-align-right"></i>
+</label>
+
+<ul class="menu">
+  <a target="_blank" href="https://www.youtube.com/channel/UCiWpg9GPsGCJTVmopI01CaA" class="item">YouTube </a>
+  <a target="blank_" href="https://www.facebook.com/Dianitaa-111165266895263/" class="item">Directos</a>
+  
+  <label for="chk" class="hide-menu-btn">
+    <i class="fas fa-arrow-circle-left"></i>
+  </label>
+</ul>
+</div>
+
+
+
+<div class="content">
+<center>
+  <img src="undraw_exciting_news_ksse(1).svg" alt="">
+</center> 
+</div>
+ <a class="gotopbtn" href="#"> <i class="fas fa-arrow-up"></i> </a>
+<div class="CEO">
+  <div class="inner">
+    <h1>INFORMACION</h1>
+    <div class="border"></div>
+
+    <div class="row">
+      <div class="col">
+        <div class="CEO">
+          <img src="Pan on Instagram_ _--__B4EOuQLgcCR(JPG)_1.jpg" alt="">
+          <div class="name">soy diana Muñoz </div>
+          <p>
+            Tengo 18 años me conocen como Pan o Dianitaa, 
+            mís directos los puedes ver en   
+            <a target="blank_" href="https://www.facebook.com/Dianitaa-111165266895263/">Facebook</a> ó
+            <a target="blank_" href="https://www.youtube.com/channel/UCiWpg9GPsGCJTVmopI01CaA">YouTube.</a>
+          </p>
+             
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="CEO">
+          <img src="IMG-20191208-WA0002.jpg" alt=""> 
+          <div class="name">💌Contacto para Negocios:</div>
+          <p>
+             pan7u7business@gmail.com ó tambien puedes llenar el formulario que estará abajo. 👇
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> 
+
+
+
+
+
+
+
+  <form name="contactform" method="post" action="enviaremail.php">
+  
+  <table width="450px">
+  
+  <tr>
+  
+   <td valign="top">
+  
+    <label for="first_name">Nombre *</label>
+  
+   </td>
+  
+   <td valign="top">
+  
+    <input  type="text" name="first_name" maxlength="50" size="30">
+  
+   </td>
+  
+  </tr>
+  
+  <tr>
+  
+   <td valign="top">
+  
+    <label for="last_name">Apellido *</label>
+  
+   </td>
+  
+   <td valign="top">
+  
+    <input  type="text" name="last_name" maxlength="50" size="30">
+  
+   </td>
+  
+  </tr>
+  
+  <tr>
+  
+   <td valign="top">
+  
+    <label for="email">E-mail *</label>
+  
+   </td>
+  
+   <td valign="top">
+  
+    <input  type="text" name="email" maxlength="80" size="30">
+  
+   </td>
+  
+  </tr>
+  
+  <tr>
+  
+   <td valign="top">
+  
+    <label for="telephone">Telefono</label>
+  
+   </td>
+  
+   <td valign="top">
+  
+    <input  type="text" name="telephone" maxlength="30" size="30">
+  
+   </td>
+  
+  </tr>
+  
+  <tr>
+  
+   <td valign="top">
+  
+    <label for="message">Mensaje *</label>
+  
+   </td>
+  
+   <td valign="top">
+  
+    <textarea  name="message" maxlength="1000" cols="25" rows="6"></textarea>
+  
+   </td>
+  
+  </tr>
+  
+  <tr>
+  
+   <td colspan="2" style="text-align:center">
+  
+    <input type="submit" value="Enviar">   <a href="http://www.tufelicidadvacacional.com.ve/email_form.php"></a>
+  
+   </td>
+  
+  </tr>
+  
+  </table>
+  
+  </form>
+  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+  
+
+
+
+     
+  <footer class="contenedor">
+        <div class="redes-sociales">
+          <div class="contenedor-icono">
+            <a href="https://twitter.com/Pan_7u7" target="_blank" class="twitter">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </div>
+          <div class="contenedor-icono">
+            <a href="https://m.facebook.com/Dianitaa-111165266895263/" target="_blank" class="facebook">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+          </div>
+          <div class="contenedor-icono">
+            <a href="https://www.instagram.com/diana.munoz__/" target="_blank" class="instagram">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </div>
+        </div>
+        <div class="creado-por">
+          <p> © Pan - By <a target="blank_" href="https://www.instagram.com/eduardo.020/">Eduardo</a></p>
+        </div>
+      </footer>
+    </div>
+      
+
+
+
+    
+</body>
+</html>
